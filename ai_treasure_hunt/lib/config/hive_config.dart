bdb11a3ecd66b8b52d212758a28e496c934fd1f1
NarayanaRaju88 <nccl.narayana@gmail.com>
2026-07-21 00:21:16 +0530

    Delete ai_treasure_hunt directory

--- ai_treasure_hunt/lib/config/hive_config.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hive_flutter/hive_flutter.dart';
-
-class HiveConfig {
-  static Future<void> initializeHive() async {
-    // Register adapters for your models here later
-    // await Hive.openBox('settings');
-  }
-}
--- ai_treasure_hunt/lib/core/di/service_locator.dart
+++
@@ -1,4 +0,0 @@
-void setupServiceLocator() {
-  // Register your repositories and services here later
-  // Example: GetIt.I.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl());
-}
